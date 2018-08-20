650b3cd4e8668825cfc150d2dd98240e2a5305ee
sonia_ramnani <sonia_ramnani@symantec.com>
2018-08-20 13:36:21 -0700

    Application to pull list of github repos

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+
+// configuration =================
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/views/index.html');
+});
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/views', express.static(__dirname + '/views'));
+
+app.use('/bower-components', express.static(__dirname + '/bower-components'));
+app.use('/css', express.static(__dirname + '/css'));
+
+
+// listen (start app with node server.js) ======================================
+app.listen(8084);
+console.log("App listening on port 8084");
